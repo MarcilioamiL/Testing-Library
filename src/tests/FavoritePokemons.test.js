91756a60bd804b71deb42cc8521bde208dc8d576
MarcilioamiL <marcilioaugusto53@gmail.com>
2022-03-23 05:05:46 -0300

    Teste o componente <FavoritePokemons.js />

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,29 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import FavoritePokemons from '../components/FavoritePokemons';
+import renderWithRouter from './helpers/renderWithRouter';
+import App from '../App';
+
+describe('Teste o componente FavoritePokemons', () => {
+  beforeEach(() => {
+    renderWithRouter(<FavoritePokemons />);
+  });
+  it(`Test se é exibido na tela a mensagem no favorite pokemon found, 
+  se a pessoa não tiver pokémons favoritos.`, () => {
+    const favoriteMensage = screen.getByText(/no favorite pokemon found/i);
+    expect(favoriteMensage).toBeDefined();
+  });
+  it('Teste se é exibido todos os cards de pokémons favoritados.', () => {
+    const { history } = renderWithRouter(<App />);
+    const details = screen.getByText(/more details/i);
+    userEvent.click(details);
+
+    const favoriteCheck = screen.getByRole('checkbox', { name: /pokémon favoritado\?/i });
+    userEvent.click(favoriteCheck);
+
+    history.push('/favorites');
+    const pokemonFavorite = screen.getByText(/Pikachu/i);
+    expect(pokemonFavorite).toBeDefined();
+  });
+});
--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,28 @@
-test('', () => {});
+// import React from 'react';
+// import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+// import renderWithRouter from './helpers/renderWithRouter';
+// import App from '../App';
+
+// describe(' Teste o componente <Pokedex.js />', () => {
+//   beforeEach(() => {
+//     renderWithRouter(<App />);
+//   });
+//   it('Teste se página contém um heading h2 com o texto Encountered pokémons', () => {
+//     const text = screen.getByRole('heading', { name: /Encountered pokémons/i, level: 2 });
+//     expect(text).toBeDefined();
+//   });
+//   it('exibe o próximo Pokémon da lista quando o botão Próximo pokémon é clicado', () => {
+//     const btn = screen.getByRole('button', { name: })
+//     console.log(screen.logTestingPlaygroundURL())
+//   });
+//   it('Teste se é mostrado apenas um Pokémon por vez', () => {
+
+//   });
+//   it('Teste se a Pokédex tem os botões de filtro', () => {
+
+//   });
+//   it('Teste se a Pokédex contém um botão para resetar o filtro', () => {
+
+//   });
+// });
