02a716768b774a394e4c2b3c9adf3e3a96a7862a
MarcilioamiL <marcilioaugusto53@gmail.com>
2022-03-23 22:27:40 -0300

    Teste o componente Pokedex.js

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,28 +1,40 @@
-// import React from 'react';
-// import { screen } from '@testing-library/react';
-// import userEvent from '@testing-library/user-event';
-// import renderWithRouter from './helpers/renderWithRouter';
-// import App from '../App';
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './helpers/renderWithRouter';
+import App from '../App';
 
-// describe(' Teste o componente <Pokedex.js />', () => {
-//   beforeEach(() => {
-//     renderWithRouter(<App />);
-//   });
-//   it('Teste se página contém um heading h2 com o texto Encountered pokémons', () => {
-//     const text = screen.getByRole('heading', { name: /Encountered pokémons/i, level: 2 });
-//     expect(text).toBeDefined();
-//   });
-//   it('exibe o próximo Pokémon da lista quando o botão Próximo pokémon é clicado', () => {
-//     const btn = screen.getByRole('button', { name: })
-//     console.log(screen.logTestingPlaygroundURL())
-//   });
-//   it('Teste se é mostrado apenas um Pokémon por vez', () => {
+describe(' Teste o componente <Pokedex.js />', () => {
+  beforeEach(() => {
+    renderWithRouter(<App />);
+  });
+  it('Teste se página contém um heading h2 com o texto Encountered pokémons', () => {
+    const text = screen.getByRole('heading', { name: /Encountered pokémons/i, level: 2 });
+    expect(text).toBeDefined();
+  });
+  it(`Teste se é exibido o próximo Pokémon da lista quando o
+  botão Próximo pokémon é clicado`, () => {
+    const btn = screen.getByRole('button', { name: /Próximo pokémon/i });
+    userEvent.click(btn);
+    const btnPokemon = screen.getByText('Charmander');
+    expect(btnPokemon).toBeDefined();
+  });
+  it('Teste se é mostrado apenas um Pokémon por vez', () => {
+    const pokemon = screen.getAllByRole('link', { name: /More details/i });
+    expect(pokemon).toHaveLength(1);
+  });
+  it('Teste se a Pokédex tem os botões de filtro', () => {
+    const number = 7;
+    const btnFilter = screen.getAllByTestId('pokemon-type-button');
+    expect(btnFilter).toHaveLength(number);
+  });
+  it('Teste se a Pokédex contém um botão para resetar o filtro', () => {
+    const btnPoko = screen.getByRole('button', { name: /próximo pokémon/i });
+    expect(btnPoko).toBeDefined();
 
-//   });
-//   it('Teste se a Pokédex tem os botões de filtro', () => {
-
-//   });
-//   it('Teste se a Pokédex contém um botão para resetar o filtro', () => {
-
-//   });
-// });
+    const allBtnsClear = screen.getByRole('button', { name: /All/i });
+    userEvent.click(allBtnsClear);
+    const allBtn = screen.getByText(/Pikachu/i);
+    expect(allBtn).toBeDefined();
+  });
+});
